552a806742be1e67f8764b1c657260524d4cbdb6
yuva neta <anuragyadavop89@gmail.com>
2026-03-18 23:47:03 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,6 +2,7 @@ from .start import cmd_start, cmd_help, callback_handler, on_new_chat_member
 from .tagging import (
     cmd_hitag, cmd_entag, cmd_gmtag, cmd_gntag,
     cmd_tagall, cmd_jtag, cmd_admin_tag, cmd_all_tag,
+    cmd_vctag,
 )
 from .control import cmd_stop, cmd_pause, cmd_resume
 from .broadcast import cmd_broadcast, cmd_stats
