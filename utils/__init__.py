0bc2554c10fa5143a6eb3aa0a27e0c3ec9f0ad33
yuva neta <anuragyadavop89@gmail.com>
2026-03-19 08:28:01 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .helpers import safe_send, safe_edit, is_admin, admin_only, owner_only, get_members, get_admin_members
-from .messages import get_msg, build_mention, GROUP_JOIN_MSG, ADMIN_TAG_PREFIX, ADMIN_TAG_SUFFIX, ALL_TAG_PREFIX, ALL_TAG_SUFFIX
+from .messages import get_msg, build_mention, build_mention_html, GROUP_JOIN_MSG, ADMIN_TAG_PREFIX, ADMIN_TAG_SUFFIX, ALL_TAG_PREFIX, ALL_TAG_SUFFIX
 from .tag_manager import tag_manager
